264a6c759cf4fd18133107b90e8597ec7bd868f6
Oleh Onyshchenko <seesmwork@gmail.com>
2023-04-14 13:22:47 +0300

    feat: add new python file

---
+++ test.py
@@ -0,0 +1,11 @@
+# Oleh Onyshchenko KHT-122
+
+import random
+
+# generate a random number between 1 and 10
+rand_num = random.randint(1, 10)
+
+# print the random number to the console
+print("Random number: {}".format(rand_num))
+
+# Oleh Onyshchenko KHT-122
